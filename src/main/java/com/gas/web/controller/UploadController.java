ac7efd2e5961a8ac6b990f0417b05dafaa70d860
jq y <137626233@qq.com>
2019-12-13 20:52:17 +0800

    upload file function

--- src/main/java/com/gas/web/controller/UploadController.java
+++ src/main/java/com/gas/web/controller/UploadController.java
@@ -1,41 +1,55 @@
 package com.gas.web.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 import java.util.logging.Logger;
 
+@Controller
 public class UploadController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
-
-    @GetMapping("/upload")
-    public String upload() {
-        return "upload";
-    }
-
-    @PostMapping("/upload")
+    /**
+     * 文件上传
+     * @param file 接收前端的formdata
+     * @return 包含上传信息的json
+     */
+    @RequestMapping(value = "/uploadJsonFile", method = RequestMethod.POST)
     @ResponseBody
-    public String upload(@RequestParam("file") MultipartFile file) {
+    public JSONObject singleFileUpload(@RequestParam("file")MultipartFile file){
+        JSONObject jsonObject = new JSONObject();
         if (file.isEmpty()) {
-            return "上传失败，请选择文件";
+            jsonObject.put("code", -1);
+            return jsonObject;
         }
-
         String fileName = file.getOriginalFilename();
-        String filePath = "/Users/itinypocket/workspace/temp/";
-        File dest = new File(filePath + fileName);
-        try {
-            file.transferTo(dest);
-            LOGGER.info("上传成功");
-            return "上传成功";
-        } catch (IOException e) {
-            LOGGER.error(e.toString(), e);
+        StringBuilder stringBuilder = new StringBuilder();
+        File fileDir = new File("src/main/resources/static/upload");
+        String path = fileDir.getAbsolutePath();
+        if(!fileDir.exists()){
+            fileDir.mkdir();
         }
-        return "上传失败！";
+        try {
+            jsonObject.put("code", 200);
+            JSONObject jsonobject = JSONObject.parseObject(new String(file.getBytes()));
+            //jsonObject.put("data", new String(file.getBytes(),"UTF-8"));
+            jsonObject.put("data", jsonobject);
+            jsonObject.put("url", stringBuilder.append(path).append(fileName).toString());
+//            file.transferTo(new File(path, fileName));
+        } catch (Exception e) {
+            jsonObject.put("code", 0);
+            e.printStackTrace();
+        }
+        return jsonObject;
     }
 }
