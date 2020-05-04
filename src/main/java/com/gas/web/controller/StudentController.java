3c7108f04af44eff2ea4a9e2ab52c828cea59516
jq y <137626233@qq.com>
2020-05-04 20:32:30 +0800

    加了批量删除 + 编辑

--- src/main/java/com/gas/web/controller/StudentController.java
+++ src/main/java/com/gas/web/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.gas.web.controller;
 
+import com.gas.web.bean.Res;
 import com.gas.web.entity.Student;
 import com.gas.web.service.IStudentService;
+import com.gas.web.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,14 +32,19 @@ public class StudentController {
     }
 
     /**
-     * 通过年龄查询学生
-     *
-     * @param enrollmentTime
+     * 通过 id 查询学生
      * @return
      */
-    @GetMapping("/{age}")
-    public List<Student> studentFindByAge(@PathVariable("age") String enrollmentTime) {
-        return studentService.studentFindByAge(enrollmentTime);
+    @GetMapping("/{id}")
+    public Response studentFindById(@PathVariable("id") Integer id) {
+        Student student = null;
+        try {
+            student = studentService.studentFindById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failure("查询失败", id);
+        }
+        return Response.success("查询成功", student);
     }
 
     /**
@@ -45,7 +52,7 @@ public class StudentController {
      * @return
      */
     @PostMapping("/add")
-    public Student studentAdd(@RequestParam("studentNum") String studentNum, @RequestParam("name") String name,
+    public Response studentAdd(@RequestParam("studentNum") String studentNum, @RequestParam("name") String name,
                               @RequestParam("sex") String sex, @RequestParam("enrollmentTime") String enrollmentTime,
                               @RequestParam("phone") String phone, @RequestParam("email") String email,
                               @RequestParam("degree") String degree, @RequestParam("type") String type,
@@ -61,41 +68,74 @@ public class StudentController {
         student.setType(type);
         student.setEmployment(employment);
         //保存和更新都用该方法
-        return studentService.studentAdd(student);
-
+        try {
+            studentService.studentAdd(student);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failure("添加失败", student);
+        }
+        return Response.success("添加成功", student);
     }
 
 
     /**
-     * 通过ID更新一个学生信息
+     * 通过 id 更新一个学生信息
      * @return
      */
-    @PutMapping("/update/{id}")
-    public Student studentUpdate(@PathVariable("id") Integer id, @RequestParam("name") String name, @RequestParam("age") Integer age,
-                                 @RequestParam("sex") String sex, @RequestParam("email") String email) {
+    @PostMapping("/update/{id}")
+    public Response studentUpdate(@PathVariable("id") Integer id,
+                                  @RequestParam("studentNum") String studentNum, @RequestParam("name") String name,
+                                  @RequestParam("sex") String sex, @RequestParam("enrollmentTime") String enrollmentTime,
+                                  @RequestParam("phone") String phone, @RequestParam("email") String email,
+                                  @RequestParam("degree") String degree, @RequestParam("type") String type,
+                                  @RequestParam("employment") String employment) {
         Student student = new Student();
         student.setId(id);
+        student.setStudentNum(studentNum);
         student.setName(name);
         student.setSex(sex);
+        student.setEnrollmentTime(enrollmentTime);
+        student.setPhone(phone);
         student.setEmail(email);
+        student.setDegree(degree);
+        student.setType(type);
+        student.setEmployment(employment);
         //保存和更新都用该方法
-        return studentService.studentUpdate(student);
+        try {
+            studentService.studentUpdate(student);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failure("编辑失败", student);
+        }
+        return Response.success("编辑成功", student);
     }
 
     /**
-     * 通过ID删除一个学生
+     * 通过 id 删除一个学生
      *
      * @param id
      */
     @DeleteMapping("/delete/{id}")
-    public void studentDelete(@PathVariable("id") Integer id) {
-        studentService.studentDelete(id);
+    public Response studentDelete(@PathVariable("id") Integer id) {
+        try {
+            studentService.studentDelete(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failure("删除失败", id);
+        }
+        return Response.success("删除成功", id);
     }
 
 
-    @PostMapping("/delete")
-    public void studentDeleteBatch(@RequestParam List<Integer> stuList) {
-        studentService.studentDeleteBatch(stuList);
+    @DeleteMapping("/delete")
+    public Response studentDeleteBatch(@RequestBody List<Student> stuList) {
+        try {
+            studentService.studentDeleteBatch(stuList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failure("删除失败", stuList);
+        }
+        return Response.success("删除成功", stuList);
     }
 
 }
